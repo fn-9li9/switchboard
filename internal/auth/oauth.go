13222bf16033d7f508ba94dc0ec7aca19a9f186e
stella <nix.os.lc@gmail.com>
2026-03-06 22:28:32 -0500

    feat(auth): add Google OAuth configuration helper
    Add NewGoogleOAuthConfig function to simplify creation of OAuth2 config
    for Google authentication with standard scopes for user profile and email.

---
+++ internal/auth/oauth.go
@@ -0,0 +1,20 @@
+package auth
+
+import (
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+func NewGoogleOAuthConfig(clientID, clientSecret, redirectURI string) *oauth2.Config {
+	return &oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		RedirectURL:  redirectURI,
+		Scopes: []string{
+			"openid",
+			"https://www.googleapis.com/auth/userinfo.email",
+			"https://www.googleapis.com/auth/userinfo.profile",
+		},
+		Endpoint: google.Endpoint,
+	}
+}
